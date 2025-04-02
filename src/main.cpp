fd767a2d67e528c3e1d13aaf534f9c3ca52dd7b9
l0ller <uday98252@gmail.com>
2025-04-02 19:55:28 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ std::string read_file_contents(const std::string &filename)
             
         }// Handle unexpected characters
     }
-    std::cout << ans; // Print the result to standard output
+    std::cerr << ans; // Print the result to standard output
     std::cout << "EOF  null" << std::endl; // End of file marker
 
     std::stringstream buffer;
