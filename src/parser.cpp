013387521acccf3bf401699a54d70496ca1aba76
l0ller <uday98252@gmail.com>
2025-04-26 03:05:03 +0530

    [any message]

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,7 +4,43 @@
 #include <string>
 #include <bits/stdc++.h>
 
-// Dummy function for now
+void parse_group(std::istringstream& iss, std::string& output_after_parse) {
+    output_after_parse += "(group ";
+    std::string content;
+    std::string line;
+
+    while (std::getline(iss, line)) {
+        std::istringstream inner_line_stream(line);
+        std::string inner_token;
+        std::vector<std::string> inner_tokens;
+
+        while (inner_line_stream >> inner_token) {
+            inner_tokens.push_back(inner_token);
+        }
+
+        if (inner_tokens.empty()) {
+            continue;
+        }
+
+        if (inner_tokens[0] == "RIGHT_PAREN") {
+            break;
+        } else if (inner_tokens[0] == "LEFT_PAREN") {
+            // Recursively parse nested parentheses
+            parse_group(iss, output_after_parse);
+        } else if (inner_tokens[0] == "STRING") {
+            std::string str = line.substr(line.find('"') + 1, line.rfind('"') - line.find('"') - 1);
+            content += str + " ";
+        } else if (inner_tokens[0] == "NUMBER" || inner_tokens[0] == "IDENTIFIER") {
+            content += inner_tokens[2] + " ";
+        } else {
+            content += line + " ";
+        }
+    }
+
+    content = content.substr(0, content.size() - 1); // Remove trailing space
+    output_after_parse += content + ")";
+}
+
 void parse_tokens(std::string&output_after_tokenize,std::string&output_after_parse) {
     std::istringstream iss(output_after_tokenize);
     std::string line;
@@ -44,31 +80,9 @@ void parse_tokens(std::string&output_after_tokenize,std::string&output_after_par
         else if(tokens[0] == "IDENTIFIER"  || tokens[0] == "STRING"){
             output_after_parse += line + "\n";
         }
-        else if(tokens[0] == "LEFT_PAREN") {
-            //Handle grouping inside parantheses
-            output_after_parse += "(group ";
-            std::string content;
-            while(std::getline(iss,line)){
-               std::istringstream inner_line_stream(line);
-               std::string inner_token;
-               std::vector<std::string> inner_tokens;
-               while(inner_line_stream >> inner_token){
-                     inner_tokens.push_back(inner_token);}
-                if(inner_tokens[0] == "RIGHT_PAREN")
-                    break;
-                if (inner_tokens[0] == "STRING") {
-                    std::string str = line.substr(line.find('"') + 1, line.rfind('"') - line.find('"') - 1);
-                    content += str + " ";
-                } else if (inner_tokens[0] == "NUMBER" || inner_tokens[0] == "IDENTIFIER") {
-                    content += inner_tokens[2] + " ";
-                } else {
-                    content += line + " ";
-                } 
-                
-            }
-            content = content.substr(0, content.size() - 1); 
-            output_after_parse += content + ")\n";
-
+        else if (tokens[0] == "LEFT_PAREN") {
+            parse_group(iss, output_after_parse);
+            output_after_parse += "\n";
         }
         else if(tokens[0] == "LEFT_PAREN (" || tokens[0] == "RIGHT_PAREN )" || tokens[0] == "LEFT_BRACE {" || tokens[0] == "RIGHT_BRACE }" || tokens[0] == "STAR *" || tokens[0] == "DOT ." || tokens[0] == "COMMA ," || tokens[0] == "SEMICOLON ;" || tokens[0] == "PLUS +" || tokens[0] == "MINUS -" || tokens[0] == "ARROW ->"){
             output_after_parse += line + "\n";
